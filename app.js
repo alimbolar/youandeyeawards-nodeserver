59a00ea41103e2cf390bfa1b6cf0ab372337d1d2
Alim Bolar <alim@fourplusmedia.com>
2022-03-22 12:42:03 +0800

    garbled up the admin route for security

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use("/admin", adminRouter);
+app.use("/sbrawiagiwmyt", adminRouter);
 
 // app.use("/", function (req, res) {
 //   res.send("This is the server for youandeyeawards");
